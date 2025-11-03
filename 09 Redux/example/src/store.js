8ac7fbe080c35fba2a7075b8776540a0fd46ca9b
Ole Ildsgaard Hougaard <oih@via.dk>
2025-11-03 08:23:12 +0100

    Simplifying the code for better readability

--- 09 Redux/example/src/store.js
+++ 09 Redux/example/src/store.js
@@ -4,15 +4,15 @@ const initState = {
   sum: 0
 }
 
-function reduce({sum} = initState, action) {
+function reduce(state = initState, action) {
   switch(action.type) {
     case 'add':
       const addend = action.payload
-      return { sum: sum + addend}
+      return { sum: state.sum + addend}
     case 'reset':
       return initState
     default:
-      return {sum}
+      return state
   }
 }
 
